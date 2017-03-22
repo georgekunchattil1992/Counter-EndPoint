f9581e0af1bcd7f0afe2776456f3026606d0163d
georgekunchattil1992 <georgekunchattil@gmail.com>
2017-03-22 23:03:55 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ button.onclick=function(){
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
-submit.onclick=function();
+submit.onclick=function(){
   //Make a request to the server and send the name
   
   //Capture a list of names and render it as a list
@@ -38,3 +38,5 @@ submit.onclick=function();
       list+='<li>'+names[i]+'</li>';
   }
   var ul=document.getElementById('namelist');
+  ul.innerHTML=list;
+};
